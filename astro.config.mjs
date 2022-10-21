90c3c1a87f08ddb84691ba881fc2f306d504fc34
stefkudla <stefankudla@gmail.com>
2022-10-21 11:33:04 -0400

    Add: sitemap plugin

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,13 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import robotsTxt from "astro-robots-txt";
-
 import image from "@astrojs/image";
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react(), image(), robotsTxt()],
-  site: "https://simple-astro-blog.vercel.app",
+  site: "https://slowheal.com/",
+  integrations: [tailwind(), react(), image(), robotsTxt(), sitemap()],
   experimental: {
     integrations: true,
   },
